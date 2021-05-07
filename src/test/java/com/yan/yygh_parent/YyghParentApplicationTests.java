f8a6f5221e82ae448c14256ed3d282250a4e6a28
baiseyan <32825107+baiseyan@users.noreply.github.com>
2021-05-07 17:31:38 +0800

    Initial commit

---
+++ src/test/java/com/yan/yygh_parent/YyghParentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yan.yygh_parent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YyghParentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
