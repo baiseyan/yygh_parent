d162de8c430df480a658c3c7cae1a1b183d9c4eb
yanjb <gzy_yan@126.com>
2021-05-07 23:02:56 +0800

    初建模块

--- service/service_hosp/src/main/java/com/yan/yygh/hosp/ServiceHospApplication.java
+++ service/service_hosp/src/main/java/com/yan/yygh/hosp/ServiceHospApplication.java
@@ -1,13 +1,11 @@
-package com.yan.yygh_parent;
+package com.yan.yygh.hosp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class YyghParentApplication {
-
+public class ServiceHospApplication {
     public static void main(String[] args) {
-        SpringApplication.run(YyghParentApplication.class, args);
+        SpringApplication.run(ServiceHospApplication.class, args);
     }
-
 }
