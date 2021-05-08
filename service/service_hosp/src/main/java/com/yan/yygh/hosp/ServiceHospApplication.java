c9d2180e9d1a8cdd7f478e76ec84dcbeb88faf0c
baiseyan <32825107+baiseyan@users.noreply.github.com>
2021-05-08 17:32:32 +0800

    测试2

--- service/service_hosp/src/main/java/com/yan/yygh/hosp/ServiceHospApplication.java
+++ service/service_hosp/src/main/java/com/yan/yygh/hosp/ServiceHospApplication.java
@@ -2,8 +2,10 @@ package com.yan.yygh.hosp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.atguigu")
 public class ServiceHospApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServiceHospApplication.class, args);
