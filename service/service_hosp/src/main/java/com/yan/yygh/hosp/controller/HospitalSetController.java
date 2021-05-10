cac98aef53847aa2819be373e27698d42788f9d1
baiseyan <32825107+baiseyan@users.noreply.github.com>
2021-05-10 20:39:37 +0800

    条件分页

--- service/service_hosp/src/main/java/com/yan/yygh/hosp/controller/HospitalSetController.java
+++ service/service_hosp/src/main/java/com/yan/yygh/hosp/controller/HospitalSetController.java
@@ -2,10 +2,14 @@ package com.yan.yygh.hosp.controller;
 
 import com.atguigu.yygh.common.result.Result;
 import com.atguigu.yygh.model.hosp.HospitalSet;
+import com.atguigu.yygh.vo.hosp.HospitalSetQueryVo;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.yan.yygh.hosp.service.HospitalSetService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -34,6 +38,26 @@ public class HospitalSetController {
             return Result.ok();
         }
             return Result.fail();
+    }
+
+    //3 条件查询带分页
+    @ApiOperation(value = "条件查询带分页")
+    @PostMapping("findPageHospitalSet/{current}/{limit}")
+    public Result findPageHospitalSet(@PathVariable long current, @PathVariable long limit, @RequestBody(required = false) HospitalSetQueryVo hospitalSetQueryVo){
+
+        Page<HospitalSet> page = new Page<>(current,limit);
+        String hoscode = hospitalSetQueryVo.getHoscode();//医院编号
+        String hosname = hospitalSetQueryVo.getHosname();//医院名称
+        QueryWrapper<HospitalSet> wrapper = new QueryWrapper<>();
+        if(!StringUtils.isEmpty(hoscode)){
+            wrapper.eq("hoscode",hoscode);
+        }
+        if(!StringUtils.isEmpty(hosname)){
+            wrapper.like("hosname",hosname);
+        }
+        Page<HospitalSet> hospitalSetPage = hospitalSetService.page(page, wrapper);
+        return Result.ok(hospitalSetPage);
+
     }
 
 }
